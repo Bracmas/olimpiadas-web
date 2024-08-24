e96c90f2e956888d0505f18efc9a801c27a9cfc3
bracmas <akamaworks@gmail.com>
2024-08-23 22:12:10 -0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	modified:   src/index.js 	.firebase/ 	.firebaserc 	firebase.json 	firestore.indexes.json 	firestore.rules 	src/controllers/ 	src/models/ 	src/screens/

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route, Routes } from "react-router-dom";
+import { Avisos } from "./screens/Avisos";
+import { Pcs } from "./screens/page";
+import { AuthProvider } from "./controllers/authContext";
+import { Register } from "./screens/Registro";
+import { Login } from "./screens/Login";
+import { ProtectedRoute } from "./screens/ProtectedRoute";
+import { CrearAviso } from "./screens/CrearAviso";
+import './screens/styles/login.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// import { Chat } from "./screens/Chat";
+
+export function App() {
+  
+  return(
+  <>
+
+  
+  <nav className=".nav">
+
+    <div className="nav-wrapper">
+      <ul className="barreichon">
+
+        <li ><Link to="/">Avisos</Link></li>
+
+      <li ><Link to="/pcs">Lista de PC</Link></li>  
+      <li>
+        <Link to="/registro">Registro</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+      <li>
+        <Link to="/crearaviso">Crear aviso</Link>
+      </li>
+
+      </ul>
+
+
+      {/* <li>
+        <Link to="/chat">Chat</Link>
+      </li> */}
     </div>
-  );
-}
+</nav>
+  <AuthProvider>
+   <Routes>
+    
+    <Route path="/" element={
+      <ProtectedRoute>
+      <Avisos/>
+      </ProtectedRoute>
+      }/>
+    <Route path="/pcs" element={
+      <ProtectedRoute>
+      <Pcs/>
+      </ProtectedRoute>
+      }/>
+    {/* <Route path="/chat" element={
+      <ProtectedRoute>
+      <Chat/>
+      </ProtectedRoute>
+      }/> */}
+    <Route path="/registro" element={<Register/>}/>
+    <Route path="/login" element={<Login/>}/>
+    <Route path="/crearaviso" element={<CrearAviso/>}/>
 
+
+
+  </Routes>
+  </AuthProvider>
+</>
+  )
+}
 export default App;
